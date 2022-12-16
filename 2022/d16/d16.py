5750249e526c0f4f5e3d7591ceb0766b91bc1792
James Dyer <jamesdyer680@gmail.com>
2022-12-16 22:13:43 +0000

    Rewrote distance, runtime down to 1 min from 4

--- 2022/d16/d16.py
+++ 2022/d16/d16.py
@@ -2,9 +2,11 @@
 
 # https://adventofcode.com/2022/day/16
 # Idea for caching max_flow and its general shape from some of the solutions on reddit.
+# After profiling, it turns out dist_between was very inefficient, so it's been replaced by Floyd-Warshall (used in many of the reddit answers)
 
 import re
-from itertools import combinations
+from collections import defaultdict
+import itertools
 from functools import cache
 from tqdm import tqdm
 
@@ -27,11 +29,12 @@ def part2(closed_valves, start="AA", minutes=26):
 # Attempt 1: Brute-force every possible split of closed valves between you and the elephant.
 # Attempt 3: Brute-force again, but this time cache the results of max_flow
 # There are 15 valves, so 2^15 splits. 
+# (It's symmetrical, so we could halve this space by not doing both (xs, ys) and (ys, xs), but the result's cached anyway)
 def all_splits(xs):
     xs = set(xs)
     l = len(xs)
     for i in range(1, l):
-        for ys in combinations(xs, i):
+        for ys in itertools.combinations(xs, i):
             ys = set(ys)
             yield (xs.difference(ys), ys)
 
@@ -39,52 +42,22 @@ def all_splits(xs):
 def max_flow(closed_valves, current, minutes):
     if minutes <= 0:
         return 0
-    flows = [0]
+    max_f = 0
     for v, f in closed_valves:
         # Your attempt at this valve
         d = dist_between(current, v)
         if d is not None and d < minutes:
             mins_remaining = minutes - (d + 1) # it takes 1 more min to open the valve
             flow = mins_remaining * f
-            flows.append(flow + max_flow(closed_valves - {(v, f)}, v, mins_remaining))
+            max_f = max(max_f, flow + max_flow(closed_valves - {(v, f)}, v, mins_remaining))
         
-    return max(flows)
+    return max_f
 
-def dist_between(start, end, distances=dict()):
-    global valves
-    # Note: unless distances is provided, this mutates the same initial dict every time
-    if not distances:
-        # Empty, first run. Init by saying the distance of each valve to itself is 0.
-        distances.update(dict.fromkeys(((v, v) for v in valves), 0))
-    d1 = distances.get((start, end), None)
-    #d2 = distances.get((end, start), None)
-    if d1 is not None:
-        # We already have the distance between these two
-        return d1
-    #if d2 is not None:
-    #    return d2
-    # Explore and add distances between nodes until we find what we want
-    depth = 1
-    (_, tunnels) = valves[start]
-    next_tunnels = set()
-    seen = {start}
-    while tunnels:
-        for t in tunnels:
-            if t in seen:
-                continue
-            distances.setdefault((start, t), depth)
-            distances.setdefault((t, start), depth)
-            d_to_end = distances.get((t, end), None)
-            if d_to_end is not None:
-                return depth + d_to_end
-            for next_t in valves[t][1]:
-                next_tunnels.add(next_t)
-        tunnels, next_tunnels = next_tunnels, set()
-        depth += 1
-    
-    print(f"Couldn't find a tunnel from {start} to {end}, searched up to depth {depth}")
-    print(distances)
-    return None
+# Now uses Floyd-Warshall! Much faster
+# This would be even faster with fewer function calls and global lookups, but I'm happy with this
+def dist_between(start, end):
+    global distances
+    return distances[(start, end)]
 
 def find_valves(lines):
     pattern = re.compile("^Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)$")
@@ -111,6 +84,18 @@ if __name__ == "__main__":
     valves = find_valves(lines)
     start = "AA"
     closed_valves = frozenset((valve, flow) for (valve, (flow, _)) in valves.items() if flow > 0) # Only the ones that have a non-zero flow
+    
+    # Floyd-Warshall to find the shortest distance between all our valves
+    global distances
+    distances = defaultdict(lambda: 100_000)
+    for (v, (_, tunnels)) in valves.items():
+        distances[(v, v)] = 0 # The distance to yourself is 0
+        for t in tunnels:
+            distances[(v, t)] = 1 # Initial edges
+            distances[(t, v)] = 1
+    for (k, i, j) in itertools.product(valves, valves, valves):
+        # Note: k ranges slowest, so all the pairs have a chance to go first
+        distances[(i, j)] = min(distances[(i, j)], distances[(i, k)] + distances[(k, j)])
 
     print("Parsing done...")
     print("---- Part 1 ----")
